633c8b300e943e7a45a9fa995f3b246b53b1d8a8
Vrindavan Sanap <vrindavansanap@gmail.com>
2024-07-01 18:04:52 +0530

    a

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -9,3 +9,52 @@
 // the screen should be cleared.
 
 //// Replace this comment with your code.
+@SCREEN
+D=A
+@addr
+M=D
+
+// n = RAM[0]
+@0
+D=M
+@n
+M=D
+
+// i = 0
+@i
+M=0
+
+@32
+D=A
+@increment
+M=D
+
+(LOOP)
+// if ((n-i)<0) goto END
+@n
+D=M 
+@i
+D=D-M 
+@END
+D;JEQ
+
+//RAM[arr+i] = -1
+@increment
+D=M
+@addr
+M=D+M
+A=M 
+M=-1
+
+//
+@i
+M=M+1
+
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
+
+
